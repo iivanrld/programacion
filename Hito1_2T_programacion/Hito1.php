7e9b9bf14189f2c33107f796746f701729392f62
iivanrld <151529210+iivanrld@users.noreply.github.com>
2024-01-16 18:36:42 +0100

    save

--- Hito1_2T_programacion/Hito1.php
+++ Hito1_2T_programacion/Hito1.php
@@ -7,20 +7,19 @@
 
 <?php
     function calcular_fianza($usuario, $semanas) {
-        $precio_hora = 15;
+        $precio_hora = 9.50;
         $sesion_privada = $usuario['horas_extra'];
         $total_de_la_sesion = $sesion_privada * $precio_hora;
 
         $categoria_precio = array(
-            'principiante' => 20,
-            'intermedia' => 25,
-            'avanzada' => 30,
+            'principiante' => 25,
+            'intermedia' => 30,
             'elite' => 35
         );
 
         $categoria_precio_fianza = $categoria_precio[$usuario['categoria']];
 
-        $evento_fianza = $usuario['eventos'] * 10;
+        $evento_fianza = $usuario['eventos'] * 22;
 
         $total_fianza = $total_de_la_sesion + $categoria_precio_fianza + $evento_fianza;
 
@@ -28,13 +27,22 @@
     }
 
     $usuarios = array(
-        array('nombre' => 'Ivan', 'plan' => 'Intermedio', 'peso' => 70, 'categoria' => 'intermedia', 'eventos' => 3, 'horas_extra' => 5),
-        array('nombre' => 'Laura', 'plan' => 'Advanzado', 'peso' => 80, 'categoria' => 'avanzada', 'eventos' => 1, 'horas_extra' => 0),
+        array('nombre' => 'Ivan', 'plan' => 'Intermedia', 'peso' => 70, 'categoria' => 'intermedia', 'eventos' => 3, 'horas_extra' => 5),
+        array('nombre' => 'Laura', 'plan' => 'intermedia', 'peso' => 80, 'categoria' => 'intermedia', 'eventos' => 1, 'horas_extra' => 0),
         array('nombre' => 'Rafa', 'plan' => 'Elite', 'peso' => 90, 'categoria' => 'elite', 'eventos' => 5, 'horas_extras' => 10),
         array('nombre' => 'Irene', 'plan' => 'Principiante', 'peso' => 60, 'categoria' => 'principiante', 'eventos' => 0, 'horas_extra' => 0),
         array('nombre' => 'Daniel', 'plan' => 'Elite', 'peso' => 100, 'categoria' => 'elite', 'eventos' => 3, 'horas_extra' => 15)
     );
 
+    $pesos = array(
+        'peso pesado'.$pesos > 100,
+        'semipesado'.$pesos > 91,
+        'peso medio'.$pesos > 81,
+        'peso medio ligero'.$pesos > 73,
+        'peso ligero'.$pesos > 66,
+        'peso pluma'.$pesos < 66,
+    );
+    
     foreach ($usuarios as $usuario) {
         $fianza = calcular_fianza($usuario, 4);
         echo "<p>Nombre: " . $usuario['nombre'] . "</p>";
