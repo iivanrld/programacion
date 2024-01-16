8849dccc63840ac4bc02fa57ba33484a45454c67
iivanrld <151529210+iivanrld@users.noreply.github.com>
2024-01-16 18:12:27 +0100

    save

--- Hito1_2T_programacion/Hito1.php
+++ Hito1_2T_programacion/Hito1.php
@@ -8,7 +8,7 @@
 <?php
     function calcular_fianza($usuario, $semanas) {
         $precio_hora = 15;
-        $sesion_privada = $usuario['extra_hours'];
+        $sesion_privada = $usuario['horas_extra'];
         $total_de_la_sesion = $sesion_privada * $precio_hora;
 
         $categoria_precio = array(
@@ -27,7 +27,7 @@
         return $total_fianza * $semanas;
     }
 
-    $usuario = array(
+    $usuarios = array(
         array('nombre' => 'Ivan', 'plan' => 'Intermedio', 'peso' => 70, 'categoria' => 'intermedia', 'eventos' => 3, 'horas_extra' => 5),
         array('nombre' => 'Laura', 'plan' => 'Advanzado', 'peso' => 80, 'categoria' => 'avanzada', 'eventos' => 1, 'horas_extra' => 0),
         array('nombre' => 'Rafa', 'plan' => 'Elite', 'peso' => 90, 'categoria' => 'elite', 'eventos' => 5, 'horas_extras' => 10),
@@ -35,7 +35,7 @@
         array('nombre' => 'Daniel', 'plan' => 'Elite', 'peso' => 100, 'categoria' => 'elite', 'eventos' => 3, 'horas_extra' => 15)
     );
 
-    foreach ($users as $user) {
+    foreach ($usuarios as $usuario) {
         $fianza = calcular_fianza($usuario, 4);
         echo "<p>Nombre: " . $usuario['nombre'] . "</p>";
         echo "<p>Plan: " . $usuario['plan'] . "</p>";
@@ -48,5 +48,40 @@
     }
 ?>
 
+<h2>Registro de clientes</h2>
+
+<form action="register.php" method="post">
+    <label for="name">Nombre:</label><br>
+    <input type="text" id="name" name="name"><br>
+    <label for="plan">Plan:</label><br>
+    <input type="text" id="plan" name="plan"><br>
+    <label for="weight">Peso:</label><br>
+    <input type="number" id="weight" name="weight"><br>
+    <label for="category">Categoria:</label><br>
+    <input type="text" id="category" name="category"><br>
+    <label for="events">Eventos:</label><br>
+    <input type="number" id="events" name="events"><br>
+    <label for="extra_hours">Horas extra:</label><br>
+    <input type="number" id="extra_hours" name="extra_hours"><br><br>
+    <input type="submit" value="Registrar">
+</form>
+
+<?php
+
+$user = array(
+    'name' => $_POST['name'],
+    'weight' => $_POST['weight'],
+    'plan' => $_POST['plan'],
+    'category' => $_POST['category'],
+    'events' => $_POST['events'],
+    'extra_hours' => $_POST['extra_hours']
+);
+
+$users[] = $user;
+
+// Redirigir al usuario a la página de inicio después de registrarlo
+header('Location: Hito1.php');
+
+?>
 </body>
 </html>
