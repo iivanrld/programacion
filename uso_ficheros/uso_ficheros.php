fd0b765feaa9bfa1586766d3a7553581f17b84a9
iivanrld <151529210+iivanrld@users.noreply.github.com>
2024-01-17 12:44:02 +0100

    save

--- uso_ficheros/uso_ficheros.php
+++ uso_ficheros/uso_ficheros.php
@@ -2,3 +2,12 @@
 //apertura de ficheros de texto
 
 $archivo = fopen("prueba.txt","w");
+
+//los modos de apertura de los ficheros son
+
+//w: abre el archivo para la escritura desde el principio
+//a: abre el archivo para la escritura desde la ultima linea que contenga
+
+// escritura en archivos de texto
+
+fwrite($archivo, "Linea añadida desde codigo al archivo de texto.");
