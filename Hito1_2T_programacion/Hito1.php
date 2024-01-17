44a3d2997cc12fa44296639f183263756632bdb7
iivanrld <151529210+iivanrld@users.noreply.github.com>
2024-01-17 18:16:51 +0100

    save

--- Hito1_2T_programacion/Hito1.php
+++ Hito1_2T_programacion/Hito1.php
@@ -90,7 +90,27 @@
 
 <html>
 <body>
-
+<div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previo</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">siguiente</span>
+  </button>
+</div>
   <h2>Introduzca los datos del usuario</h2>
 
   <form action="register.php" method="post">
