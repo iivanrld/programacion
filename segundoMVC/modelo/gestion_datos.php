50aa75c27db4ecb8e50c4db17ebd6bbeb42c6b0a
iivanrld <151529210+iivanrld@users.noreply.github.com>
2024-01-22 14:05:00 +0100

    save

--- segundoMVC/modelo/gestion_datos.php
+++ segundoMVC/modelo/gestion_datos.php
@@ -1,3 +1,32 @@
 <?php
 require_once("control.php");
 $archivo = fopen("datos.dat","w");
+// Recuperar los datos del formulario
+$nombre = $_POST['nombre'];
+$apellidos = $_POST['apellidos'];
+
+fwrite($archivo,'Hola '.$nombre."".$apellidos.PHP_EOL);
+
+$archivo = fopen("datos.dat","a");
+
+while (feof($archivo)) {
+    $linea = fgets($archivo);
+    echo $linea;
+}
+
+fclose($archivo);
+
+$nombre_archivo = "datos.dat";
+
+if (file_exists($nombre_archivo)){
+    $archivo = fopen($nombre_archivo, "r");
+
+    while (feof($archivo)) {
+        $linea = fgets($archivo);
+        echo $linea;
+    }
+    fclose($archivo);
+}else{
+    echo "El archivo que estas intentando leer no existe";
+}
+
