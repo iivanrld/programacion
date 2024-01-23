41ebb13ca7898dd3c89d327b34ca1f169cd223f2
iivanrld <151529210+iivanrld@users.noreply.github.com>
2024-01-23 14:05:37 +0100

    save

--- tercerMVC/tabla_datos.php
+++ tercerMVC/tabla_datos.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link ref="stylesheet" href="style.css">
-    <title>Tabla de datos del formulairo</title>
+    <title>Tabla de datos del formulario</title>
 </head>
 <body>
     
 </body>
 </html>
+
+<?php
+require("formulario.php");
+$nombre = $_POST['nombre'];
+$apellidos = $_POST['apellidos'];
+
