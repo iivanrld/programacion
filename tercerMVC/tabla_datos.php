abefb44a902ef381a80a9468e4a3101e1c1db070
iivanrld <151529210+iivanrld@users.noreply.github.com>
2024-01-23 14:09:53 +0100

    save

--- tercerMVC/tabla_datos.php
+++ tercerMVC/tabla_datos.php
@@ -7,12 +7,30 @@
     <title>Tabla de datos del formulario</title>
 </head>
 <body>
-    
+<table>
+        <tr>
+            <th>Nombre</th>
+            <th>Edad</th>
+            <th>Email</th>
+        </tr>
+        <?php
+        $filename = 'datos.dat';
+        $file = fopen($filename, 'r');
+        while (($line = fgets($file)) !== false) {
+            $data = explode(',', $line);
+            echo "<tr>";
+            echo "<td>{$data[0]}</td>";
+            echo "<td>{$data[1]}</td>";
+            echo "<td>{$data[2]}</td>";
+            echo "</tr>";
+        }
+        fclose($file);
+        ?>
+    </table>
+
+    <form action="index.php" method="get">
+        <input type="submit" value="Volver al menú principal">
+    </form>
 </body>
 </html>
-
-<?php
-require("formulario.php");
-$nombre = $_POST['nombre'];
-$apellidos = $_POST['apellidos'];
 
