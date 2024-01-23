431f1d67df2797990425ed95ef29524da47bc751
iivanrld <151529210+iivanrld@users.noreply.github.com>
2024-01-23 13:29:29 +0100

    save

--- segundoMVC/controlador/control.php
+++ segundoMVC/controlador/control.php
@@ -11,3 +11,18 @@ if (empty($nombre) || empty($apellidos)) {
     echo "¡Gracias, $nombre! Sus apellidos son $apellidos.";
 }
 
+fwrite($archivo,'Hola '.$nombre."".$apellidos.PHP_EOL);
+
+$nombre_archivo = "datos.dat";
+
+if (file_exists($nombre_archivo)){
+    $archivo = fopen($nombre_archivo, "r");
+
+    while (feof($archivo)) {
+        $linea = fgets($archivo);
+        echo $linea;
+    }
+    fclose($archivo);
+}else{
+    echo "El archivo que estas intentando leer no existe";
+}
